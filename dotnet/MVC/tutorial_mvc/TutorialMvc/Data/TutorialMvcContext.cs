cebd7db5ef2de25b277061b4a5ec8f34380733ef
Lucas <lvksounds@gmail.com>
2022-09-22 10:20:12 -0300

    novos estudos

---
+++ dotnet/MVC/tutorial_mvc/TutorialMvc/Data/TutorialMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TutorialMvc.Models;
+
+namespace TutorialMvc.Data
+{
+    public class TutorialMvcContext : DbContext
+    {
+        public TutorialMvcContext (DbContextOptions<TutorialMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
